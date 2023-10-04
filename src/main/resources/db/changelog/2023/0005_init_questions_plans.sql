e2b31cfc687089a3c16b2071768a8828447813dd
Andrew Kuskov <qvant86@gmail.com>
2023-10-04 03:10:52 +0300

    more questions

--- src/main/resources/db/changelog/2023/0005_init_questions_plans.sql
+++ src/main/resources/db/changelog/2023/0005_init_questions_plans.sql
@@ -10,5 +10,12 @@ update questions set correct_answer_id = currval('s_answers') where id = currval
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Explain analyze gather stats before making plan', currval('s_questions'));
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Explain analyze provide better plan', currval('s_questions'));
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'They are the same', currval('s_questions'));
+insert into questions(id, text, level_id, category_id) values (nextval('s_questions'), 'What the Index Scan does?', 1, currval('s_categories'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Read the whole index', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Go from the root to the only accepted value, if any', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Go from the root to the first leaf node with accepted value, than walk through leafs until condition is false without following table access', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Go from the root to the first leaf node with accepted value, than walk through leafs until condition is false, than access table pages, based on index info', currval('s_questions'));
+update questions set correct_answer_id = currval('s_answers') where id = currval('s_questions');
+
 
 commit;
