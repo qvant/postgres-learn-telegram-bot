7fd1a67af11386e6e81b1624a6a710b15c199df7
Andrew Kuskov <qvant86@gmail.com>
2023-12-05 01:13:44 +0300

    more questions

--- src/main/resources/db/changelog/2023/0006_init_questions_indexes.sql
+++ src/main/resources/db/changelog/2023/0006_init_questions_indexes.sql
@@ -40,7 +40,7 @@ update questions set correct_answer_id = currval('s_answers') where id = currval
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'TID', currval('s_questions'));
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Double-linked pages list pointers', currval('s_questions'));
 
-insert into questions(id, text, level_id, category_id) values (nextval('s_questions'), 'What types of pages hash index has?', 1, currval('s_categories'));
+insert into questions(id, text, level_id, category_id) values (nextval('s_questions'), 'Which types of pages hash index has?', 1, currval('s_categories'));
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Root and leaf', currval('s_questions'));
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Meta and backet', currval('s_questions'));
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Meta, backet, overflow and bitmap', currval('s_questions'));
@@ -52,6 +52,41 @@ insert into answers(id, text, question_id) values (nextval('s_answers'), 'Page, 
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Additional index meta information', currval('s_questions'));
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Part of index keys that don''t fit backet key', currval('s_questions'));
 insert into answers(id, text, question_id) values (nextval('s_answers'), 'Extras pages that keep pairs (hash, tid), which don''t fit backet page' , currval('s_questions'));
+update questions set correct_answer_id = currval('s_answers') where id = currval('s_questions');
+
+insert into questions(id, text, level_id, category_id) values (nextval('s_questions'), 'What an bottom-up index deletion?', 1, currval('s_categories'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Operation of index removal, called on drop index', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Cleaning index pages from dead records', currval('s_questions'));
+update questions set correct_answer_id = currval('s_answers') where id = currval('s_questions');
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Index maintenance operation, performed when tuple deleted from table', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Index maintenance operation, performed on vacuum' , currval('s_questions'));
+
+insert into questions(id, text, level_id, category_id) values (nextval('s_questions'), 'What an simple index tuple deletion?', 1, currval('s_categories'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Operation of index removal, called on drop index', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Index maintenance operation, performed when tuple deleted from table', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Postponed cleaning index pages from dead records with LP_DEAD flag', currval('s_questions'));
+update questions set correct_answer_id = currval('s_answers') where id = currval('s_questions');
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Index maintenance operation, performed on vacuum' , currval('s_questions'));
+
+insert into questions(id, text, level_id, category_id) values (nextval('s_questions'), 'Does Postgres index NULL values?', 1, currval('s_categories'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Yes', currval('s_questions'));
+update questions set correct_answer_id = currval('s_answers') where id = currval('s_questions');
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'No', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Only when it''s part of multicolumn index with  al lease one not-null column value', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Only when index created with STORE NULL clause' , currval('s_questions'));
+
+insert into questions(id, text, level_id, category_id) values (nextval('s_questions'), 'Does Postgres combine duplicate index keys?', 1, currval('s_categories'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Yes, always', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'No, never', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Only when index are creating or record doesn''t fit index page', currval('s_questions'));
+update questions set correct_answer_id = currval('s_answers') where id = currval('s_questions');
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Only when index are creating' , currval('s_questions'));
+
+insert into questions(id, text, level_id, category_id) values (nextval('s_questions'), 'Does Postgres allow to keep extra columns, which are doesn''t part of key?', 1, currval('s_categories'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'No', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Yes, when index organized table are created', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Only when OVERFLOW clause used', currval('s_questions'));
+insert into answers(id, text, question_id) values (nextval('s_answers'), 'Yes, when INCLUDE clause used' , currval('s_questions'));
 update questions set correct_answer_id = currval('s_answers') where id = currval('s_questions');
 
 commit;
